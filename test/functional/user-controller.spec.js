259210c6fef13673951aa0227c929d1e597525a6
NoManColonies <katsunari0908@gmail.com>
2020-12-13 00:09:59 +0700

    Added cleanup code after user creation test.

--- test/functional/user-controller.spec.js
+++ test/functional/user-controller.spec.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { test, trait } = use('Test/Suite')('User Controller')
+const UserModel = use('App/Models/User')
 
 trait('Test/ApiClient')
 trait('Auth/Client')
@@ -13,6 +14,8 @@ test('should return status message of success upon user instance creation.', asy
     .header('username', 'test')
     .header('password', 'password')
     .end()
+
+  await UserModel.first().then(query => query.delete())
 
   response.assertStatus(201)
   response.assertJSONSubset({ status: 'success' })
