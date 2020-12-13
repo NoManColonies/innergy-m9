36ac0318f58c7e62a78dd5d67d2b732cd9b44531
NoManColonies <katsunari0908@gmail.com>
2020-12-13 11:22:03 +0700

    Optimised cleanup code after ease test suite.

--- test/functional/user-controller.spec.js
+++ test/functional/user-controller.spec.js
@@ -2,20 +2,36 @@
 
 const { test, trait } = use('Test/Suite')('User Controller')
 const UserModel = use('App/Models/User')
+const TokenModel = use('App/Models/Token')
+const Encrytion = use('Encrytion')
 
 trait('Test/ApiClient')
 trait('Auth/Client')
 
 const urlEndPoint = '/api/v1/user'
 
+const cleanUp = async ({ user, token }) => {
+  if (user) {
+    await UserModel.findBy({ username: user }).then(
+      query => !!query && query.delete()
+    )
+  }
+  if (token) {
+    await TokenModel.findBy({ token: await Encrytion.decrypt(token) }).then(
+      query => !!query && query.delete()
+    )
+  }
+}
+
 test('should return status message of success upon user instance creation.', async ({ client }) => {
   const response = await client
     .post(`${urlEndPoint}/create`)
     .header('username', 'test')
     .header('password', 'password')
+    .header('email', 'example@domain.host')
     .end()
 
-  await UserModel.findBy({ u_id: response.body.user.u_id }).then(query => query.delete())
+  cleanUp({ user: 'test' })
 
   response.assertStatus(201)
   response.assertJSONSubset({ status: 'success' })
