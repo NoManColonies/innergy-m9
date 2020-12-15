b595806a3ad4d8a2f6d2f1f2c0dab87f387280fa
NoManColonies <katsunari0908@gmail.com>
2020-12-15 11:09:31 +0700

    Removed user unit test.

--- test/unit/user-utils.spec.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const { test } = use('Test/Suite')('User Utils')
-const UserModel = use('App/Models/User')
-
-test('should return instance of created user.', async ({ assert }) => {
-  assert.equal(2 + 2, 4)
-})
